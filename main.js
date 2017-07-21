b57fe4ce0e378a4d3011c818952c318e941cabed
清锐 <lf101548@alibaba-inc.com>
2017-07-21 19:52:08 +0800

    update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray} = require('electron')
 const path = require('path')
 const url = require('url')
+const assetsDirectory = path.join(__dirname, 'assets')
+const ip = require('ip')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+let tray
 
 function createWindow () {
   // Create the browser window.
@@ -29,10 +32,39 @@ function createWindow () {
   })
 }
 
+const createTray = () => {
+  tray = new Tray(path.join(assetsDirectory, 'tray.jpg'))
+  tray.setTitle(ip.address())
+  tray.on('right-click', toggleWindow)
+  tray.on('double-click', toggleWindow)
+  tray.on('click', function (event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (win.isVisible() && process.defaultApp && event.metaKey) {
+      win.openDevTools({mode: 'detach'})
+    }
+  })
+}
+
+const toggleWindow = () => {
+  if (win.isVisible()) {
+    win.hide()
+  } else {
+    win.show()
+    win.focus()
+  }
+}
+
+app.dock.hide()
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  createTray()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
